25d3ed4d8daf16a7e862ede4408cc37378e7501b
Adrian Bueno <bueno1dad@gmail.com>
2021-08-21 17:10:23 -0300

    up montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-        backgroundColor: ['active'],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontfamily: {
+            'sans': ['Montserrat']
+        },
+        extend: {},
     },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        },
+    },
+    plugins: [],
 }

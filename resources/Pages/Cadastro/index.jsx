aaa98acb2c503945ebd64194a6859bdfe2720ea8
adrian7123 <bueno1dad@gmail.com>
2021-08-22 20:49:16 -0300

    register

--- resources/Pages/Cadastro/index.jsx
+++ resources/Pages/Cadastro/index.jsx
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from '@inertiajs/inertia-react'
+import Axios from 'axios'
 
 class index extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
     render() {
+
+        const registrar = async (e) => {
+            e.preventDefault();
+
+            let form = this.myRef.current
+
+            let data = await Axios.post(route('user.register'), {
+                name: form[0].value,
+                email: form[1].value,
+                password: form[2].value,
+            }).then(res => res.data)
+        }
         return (
             <div className="grid w-screen h-screen place-items-center bg-primary">
-                <form className="border border-quinary rounded bg-secondary px-8 pt-6 pb-8 mb-4">
+                <form ref={this.myRef} onSubmit={registrar} className="border border-quinary rounded bg-secondary px-8 pt-6 pb-8 mb-4">
 
                     <div className="mb-4">
-                    <label className="block text-quinary text-sm font-bold mb-2" for="email"> Nome de Usuário </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Seu @ aqui :3"/>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="username"> Nome de Usuário </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Seu @ aqui :3" name="name"/>
                     </div>
 
                     <div className="mb-4">
-                    <label className="block text-quinary text-sm font-bold mb-2" for="email"> E-mail </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Seu e-mail aqui :D"/>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="email"> E-mail </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Seu e-mail aqui :D"/>
                     </div>
 
                     <div className="mb-4">
-                    <label className="block text-quinary text-sm font-bold mb-2" for="password">Senha</label>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="password">Senha</label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Sua senha aqui :D"/>
 
-                    <label className="block text-quinary text-sm font-bold mb-2" for="password">Confirme sua senha</label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Repita sua senha :D"/>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="confirm_password">Confirme sua senha</label>
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirm_password" type="password" placeholder="Repita sua senha :D"/>
                     <p className="text-red-500 text-xs italic">Senha incorreta.</p>
                     </div>
 
                     <div className="mb-4 flex justify-center">
-                    <button className="bg-quinary hover:bg-quaternary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button className="bg-quinary hover:bg-quaternary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Próximo
                     </button>
                     </div>
--- resources/Pages/Login/index.jsx
+++ resources/Pages/Login/index.jsx
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import { Link } from '@inertiajs/inertia-react'
 
 class index extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
     render() {
         return (
             <div className="grid w-screen h-screen place-items-center bg-primary">
-                <form className="border border-quinary rounded bg-secondary px-8 pt-6 pb-8 mb-4">
+                <form ref={this.myRef} onSubmit={(e) => e.preventDefault()} className="border border-quinary rounded bg-secondary px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
-                    <label className="block text-quinary text-sm font-bold mb-2" for="username"> E-mail </label>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="username"> E-mail </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Seu e-mail aqui :D"/>
                     </div>
 
                     <div className="mb-4">
-                    <label className="block text-quinary text-sm font-bold mb-2" for="password">Senha</label>
+                    <label className="block text-quinary text-sm font-bold mb-2" htmlFor="password">Senha</label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Sua senha aqui :D"/>
                     <p className="text-red-500 text-xs italic">Digite sua senha.</p>
                     </div>
 
                     <div className="mb-4 flex justify-center">
-                    <button className="bg-quinary hover:bg-quaternary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button className="bg-quinary hover:bg-quaternary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Entrar
                     </button>
                     </div>
